4568e17698e349e58a2353f0595cee97d6d6ed70
BertoldVinczeIMC <21imc10072@fh-krems.ac.at>
2024-04-08 19:48:46 +0200

    Lookaround in data

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ LINK = "https://www.pnas.org/doi/10.1073/pnas.211566398"
 def main() -> int:
     ds = pl.read_csv("data/GCM_Normal.res", separator="\t")
 
+    print(ds.head())
     print(ds.describe())
 
     return 0
